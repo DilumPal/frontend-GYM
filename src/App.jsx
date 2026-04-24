ba3a3d60a82d2e327b42972e0c84679bef3076da
DilumPal <palawaththadilum@gmail.com>
2026-04-24 09:04:35 +0530

    register page complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import LoginPage from './pages/login'
-import SignupPage from './pages/signup'
 import HomePage from './pages/home'
 import Header from './components/header'
 import AdminPage from './pages/adminPage'
 import TestPage from './pages/testPage'
 import { Toaster } from 'react-hot-toast'
+import RegisterPage from './pages/register'
 
 function App() {
   
@@ -18,7 +18,7 @@ function App() {
         <Routes path="/">
           <Route path="/" element={<HomePage/>} />
           <Route path="/login" element={<LoginPage/>} />
-          <Route path="/signup" element={<SignupPage/>} />
+          <Route path="/signup" element={<RegisterPage/>} />
           <Route path="testPage" element={<TestPage/>}/>
           <Route path="/adminPage*" element={<AdminPage/>} />
           <Route path="/*" element={<h1>404 not found</h1>}/>
---
+++ src/pages/register.jsx
@@ -0,0 +1,85 @@
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+export default function RegisterPage() {
+
+    const [email, setEmail] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate("");
+
+    async function handleRegister() {
+        try {
+            const response = await axios.post(
+                import.meta.env.VITE_BACKEND_URL + "/api/users",
+                {
+                    email: email,
+                    firstName: firstName,
+                    lastName: lastName,
+                    password: password
+                    // role, isBlocked, img → will take default values from schema
+                }
+            );
+
+            toast.success("Registration Successful");
+
+            navigate("/login");
+
+        } catch (e) {
+            toast.error(e.response?.data?.message || "Registration Failed");
+        }
+    }
+
+    return (
+        <div className="w-full h-screen bg-[url('/login.jpg')] bg-center bg-cover flex justify-center items-center">
+            
+            <div className="w-[50%] h-full"></div>
+
+            <div className="w-[50%] h-full flex justify-center items-center">
+                <div className="w-[500px] h-[650px] backdrop-blur-md rounded-[20px] shadow-xl flex flex-col justify-center items-center">
+
+                    <input
+                        onChange={(e) => setFirstName(e.target.value)}
+                        value={firstName}
+                        placeholder="First Name"
+                        className="w-[300px] h-[50px] border border-white rounded-[20px] my-[10px] px-4"
+                    />
+
+                    <input
+                        onChange={(e) => setLastName(e.target.value)}
+                        value={lastName}
+                        placeholder="Last Name"
+                        className="w-[300px] h-[50px] border border-white rounded-[20px] my-[10px] px-4"
+                    />
+
+                    <input
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        placeholder="Email"
+                        className="w-[300px] h-[50px] border border-white rounded-[20px] my-[10px] px-4"
+                    />
+
+                    <input
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        type="password"
+                        placeholder="Password"
+                        className="w-[300px] h-[50px] border border-white rounded-[20px] my-[10px] px-4"
+                    />
+
+                    <button
+                        onClick={handleRegister}
+                        className="w-[300px] h-[50px] cursor-pointer bg-gray-400 rounded-[20px] text-[20px] font-bold my-[20px]"
+                    >
+                        Register
+                    </button>
+
+                </div>
+            </div>
+        </div>
+    );
+}
