9e0d6f2f19532f4571b1b93030f886d1746070d6
DilumPal <palawaththadilum@gmail.com>
2026-04-25 22:28:08 +0530

    connect supabase

--- src/pages/testPage.jsx
+++ src/pages/testPage.jsx
@@ -1,5 +1,25 @@
+import { createClient } from "@supabase/supabase-js";
 import { useState } from "react"
 
 export default function TestPage(){
-    
+
+    const [image, setImage] = useState(null);
+
+    const url = "https://tgjwaeillwyegfbedncz.supabase.co/rest/v1/"
+
+    const supabase = createClient(url)
+
+    function fileUpload(){
+        console.log("Button clicked");
+    }
+
+    return(
+        <div className="w-full h-screen flex justify-center items-center flex-col">
+            <input type="file" className="file-input file-input-bordered w-full max-w-xs"
+            onChange={(e)=>{
+                setImage(e.target.files(0))
+            }}/>
+            <button onClick={fileUpload} className="bg-green-400 text-white font-bold py-2 px-4 rounded">Upload</button>
+        </div>
+    )
 }
