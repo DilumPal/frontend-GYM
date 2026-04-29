b86b111f8ca4b04f1263b786ed1d781b15c489b1
DilumPal <palawaththadilum@gmail.com>
2026-04-29 18:43:48 +0530

    Header UI fix

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,8 +3,14 @@ import UserData from "./userData";
 
 export default function Header(){
     return(
-        <div className="w-full h-[80px] shadow-2xl" >
-            <img src="/logo.png" alt="Logo" className="w-[80px] h-[80px] object-cover absolute top-0 left-0 m-2" />
-        </div>
+        <header className="w-full h-[80px] shadow-2xl flex" >
+            <img src="/logo.png" alt="Logo" className="w-[80px] h-[80px] object-cover cursor-pointer top-0 left-0 m-2" />
+            <div className="w-[calc(100%-160px)] h-full bg-green-600">
+                
+            </div>
+            <div className="w-[80px] bg-green-300">
+
+            </div>
+        </header>
     )
 }
