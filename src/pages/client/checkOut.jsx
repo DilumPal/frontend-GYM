a26413ddd215d64c3458eafd469c46cfd9635752
DilumPal <palawaththadilum@gmail.com>
2026-05-04 21:28:29 +0530

    Order placing function

--- src/pages/client/checkOut.jsx
+++ src/pages/client/checkOut.jsx
@@ -2,6 +2,8 @@ import { useState } from "react"
 import { BiPlus, BiMinus, BiTrash } from "react-icons/bi";
 import { Link, useLocation } from "react-router-dom";
 import { removeFromCart } from "../../utils/cart";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 export default function CheckoutPage() {
     const location = useLocation()
@@ -9,7 +11,7 @@ export default function CheckoutPage() {
 
     const [cart, setCart] = useState(location.state?.cart || []);
 
-    function getTotal(){
+    function getTotal() {
         let total = 0;
         cart.forEach((item) => {
             total += item.price * item.qty
@@ -17,20 +19,56 @@ export default function CheckoutPage() {
         return total
     }
 
-    function removeFromCart(index){
-        const newCart = cart.filter((item,i)=>i != index)
+    function removeFromCart(index) {
+        const newCart = cart.filter((item, i) => i != index)
         setCart(newCart)
     }
 
-    function changeQty(index, qty){
+    function changeQty(index, qty) {
         const newQty = cart[index].qty + qty
-        if(newQty <= 0){
+        if (newQty <= 0) {
             removeFromCart(index)
             return
-        }else{
+        } else {
             const newCart = [...cart]
             cart[index].qty = newQty
             setCart(newCart)
+        }
+    }
+
+    async function placeOrder() {
+        const token = localStorage.getItem("token")
+        if (!token) {
+            toast.error("Please login to place an order")
+            return
+        }
+
+        const orderInformation = {
+            products : [],
+            phone : '+94772185126',
+            address : "123, main street, colombo"
+        }
+
+        for (let i = 0; i < cart.length; i++) {
+            const item = {
+                productID: cart[i].productID,
+                qty: cart[i].qty
+            }
+            orderInformation.products[i] = item
+        }
+
+        try {
+            const res = await axios.post(import.meta.env.VITE_BACKEND_URL + "/api/orders", orderInformation, {
+                headers: {
+                    Authorization: "Bearer" + token
+                }
+            })
+            toast.success("Order placed successfully")
+            console.log(res.data)
+        } catch (err) {
+            console.log(err)
+            toast.error("Error placing order")
+            return
         }
     }
 
@@ -42,13 +80,13 @@ export default function CheckoutPage() {
                         Rs {getTotal().toFixed(2)}
                     </span>
                 </p>
-                <button className="text-white bg-accent px-4 py-1 rounded-lg font-bold hover:bg-accent/80 transition-all duration-300">
+                <button className="text-white bg-accent px-4 py-1 rounded-lg font-bold cursor-pointer hover:bg-accent/80 transition-all duration-300" onClick={placeOrder}>
                     Place Order
                 </button>
             </div>
             {
                 cart.map(
-                    (item , index) => {
+                    (item, index) => {
                         return (
                             <div key={item.productID} className="w-[600px] h-[100px] rounded-tl-3xl my-4 rounded-bl-3xl bg-primary shadow-2xl flex flex-row relative justify-center items-center">
                                 <img src={item.image} className="w-[100px] h-[100px] object-cover rounded-3xl" />
@@ -66,21 +104,21 @@ export default function CheckoutPage() {
                                     }
                                 </div>
                                 <div className="flex flex-row items-center justify-center w-[100px] h-full">
-                                    <button aria-label="Decrease quantity"className="flex items-center justify-center w-[30px] h-[30px] mx-4 text-3xl text-white transition-all duration-300 bg-green-500 cursor-pointer rounded-xl hover:bg-green-600/80 active:scale-90" onClick={()=>{
+                                    <button aria-label="Decrease quantity" className="flex items-center justify-center w-[30px] h-[30px] mx-4 text-3xl text-white transition-all duration-300 bg-green-500 cursor-pointer rounded-xl hover:bg-green-600/80 active:scale-90" onClick={() => {
                                         changeQty(index, -1)
                                     }}><BiMinus /></button>
                                     <span className="mx text-2xl font-semibold text-secondary">{item.qty}</span>
-                                    <button aria-label="Increase quantity"className="flex items-center justify-center w-[30px] h-[30px] mx-4 text-3xl text-white transition-all duration-300 bg-green-500 cursor-pointer rounded-xl hover:bg-green-600/80 active:scale-90" onClick={()=>{
+                                    <button aria-label="Increase quantity" className="flex items-center justify-center w-[30px] h-[30px] mx-4 text-3xl text-white transition-all duration-300 bg-green-500 cursor-pointer rounded-xl hover:bg-green-600/80 active:scale-90" onClick={() => {
                                         changeQty(index, 1)
                                     }}><BiPlus /></button>
                                 </div>
                                 <div className="w-[200px] h-full flex flex-col justify-center items-end pr-4">
-                                    <h1 className="text-2xl text-secondary font-semibold">Rs {(item.price*item.qty).toFixed(2)}</h1>
+                                    <h1 className="text-2xl text-secondary font-semibold">Rs {(item.price * item.qty).toFixed(2)}</h1>
                                 </div>
-                                <button className="absolute cursor-pointer text-red-500 rounded-full p-2 right-[-40px] transition-all duration-300 hover:bg-red-600/80 active:scale-90 hover:text-white" onClick={()=>{
+                                <button className="absolute cursor-pointer text-red-500 rounded-full p-2 right-[-40px] transition-all duration-300 hover:bg-red-600/80 active:scale-90 hover:text-white" onClick={() => {
                                     removeFromCart(index)
                                 }}>
-                                    <BiTrash/>
+                                    <BiTrash />
                                 </button>
                             </div>
                         )
