e06137781f61366b0110cb93417db60bb5d2354e
DilumPal <palawaththadilum@gmail.com>
2026-05-09 23:43:13 +0530

    Add some mobile response view

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,22 +1,48 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import UserData from "./userData";
 import { IoCart } from "react-icons/io5";
+import { GiHamburgerMenu } from "react-icons/gi"
+import { useState } from "react";
 
 export default function Header(){
+    const [sideDrawerOpened, setSideDrawerOpened] = useState(true)
+    const navigate = useNavigate()
+
     return(
-        <header className="w-full h-[80px] shadow-2xl flex" >
+        <header className="w-full h-[80px] shadow-2xl flex justify-center relative" >
+            <GiHamburgerMenu className="h-full mx-2 text-3xl md:hidden absolute left-2" onClick={()=>{
+                setSideDrawerOpened(true)
+            }}/>
             <img src="/logo.png" alt="Logo" className="w-[80px] h-[80px] object-cover cursor-pointer top-0 left-0 m-2" />
-            <div className="w-[calc(100%-160px)] h-full bg-accent flex justify-center items-center">
+            <div className="w-[calc(100%-160px)] h-full bg-accent hidden md:flex justify-center items-center">
                 <Link to="/" className= "text-[20px] font-bold mx-2">Home</Link>
                 <Link to="/products" className="text-[20px] font-bold mx-2">Products</Link>
                 <Link to="/about" className="text-[20px] font-bold mx-2">About</Link>
                 <Link to="/contact" className="text-[20px] font-bold mx-2">Contact</Link>
             </div>
-            <div className="w-[80px] bg-green-300 flex justify-center items-center">
+            <div className="w-[80px] bg-green-300 hidden md:flex justify-center items-center">
                 <Link to="/cart" className="text-[20px] font-bold mx-2">
                     <IoCart/>
                 </Link>
             </div>
+            {
+                sideDrawerOpened&&
+                <div className="fixed h-screen w-full bg-[#00000060] flex md:hidden">
+                    <div className="w-[250px] bg-white h-full">
+                        <div className="w-full h-[80px] shadow-2xl flex justify-center items-center relative">
+                            <GiHamburgerMenu className="h-full text-3xl absolute left-2 cursor-pointer" onClick={()=>{
+                                setSideDrawerOpened(false)
+                            }}/>
+                            <img onClick={()=>{
+                                window.location.href = "/"
+                            }} src="/logo.png" alt="Logo" className="w-[80px] h-[80px] object-cover cursor-pointer"/>
+                        </div>
+                        <div className="w-full h-[calc(100%-80px)] flex flex-col items-center gap-2">
+
+                        </div>
+                    </div>
+                </div>
+            }
         </header>
     )
 }
--- src/pages/testPage.jsx
+++ src/pages/testPage.jsx
@@ -1,30 +1,9 @@
-import { createClient } from "@supabase/supabase-js";
-import { useState } from "react"
-import meadiaUpload from "../utils/meadiaUpload";
 
 export default function TestPage(){
 
-    const [image, setImage] = useState(null);
-
-
-    function fileUpload(){
-        meadiaUpload(image).then(
-            (res)=>{
-                console.log(res)
-            }
-        ).catch(
-            (res)=>{
-                console.log(res)
-        })
-    }
-
     return(
         <div className="w-full h-screen flex justify-center items-center flex-col">
-            <input type="file" className="file-input file-input-bordered w-full max-w-xs"
-            onChange={(e)=>{
-                setImage(e.target.files[0])
-            }}/>
-            <button onClick={fileUpload} className="bg-green-400 text-white font-bold py-2 px-4 rounded">Upload</button>
+            
         </div>
     )
 }
