f879b17f0afe3f5aca7b9ce45f15d33be3738194
DilumPal <palawaththadilum@gmail.com>
2026-05-17 17:06:18 +0530

    Add permission to admin page

--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -4,21 +4,55 @@ import AdminProductPage from "./admin/productPage";
 import AddProductPage from "./admin/addProduct";
 import EditProductPage from "./admin/editProductPage";
 import AdminOrdersPage from "./admin/adminOrdersPage";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import Loading from "../components/loding";
 
-export default function AdminPage(){
+export default function AdminPage() {
     const location = useLocation();
     const path = location.pathname;
+    const [status, setStatus] = useState("Loading...");
 
-    function getClass(name){
-        if(path.includes(name)){
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (!token) {
+            setStatus("Unauthorized");
+            window.location.href = "/login";
+            return;
+        } else {
+            axios.get(import.meta.env.VITE_BACKEND_URL + "/api/users/", {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }).then((response) => {
+                if (response.data.role !== "admin") {
+                    setStatus("Unauthorized");
+                    toast.error("You are not authorized to access this page");
+                    window.location.href = "/login";
+                } else {
+                    setStatus("Authorized");
+                }
+            }).catch((error) => {
+                setStatus("Unauthorized");
+                window.location.href = "/login";
+            });
+        }
+    }, [status]);
+
+    function getClass(name) {
+        if (path.includes(name)) {
             return "bg-primary text-accent p-4"
-        }else{
+        } else {
             return "text-white p-4 hover:bg-primary hover:text-accent transition-all duration-300 cursor-pointer"
         }
     }
 
-    return(
+    return (
+        
         <div className="w-full h-screen flex bg-primary overflow-hidden">
+            {status === "Loading"? <Loading /> :
+            <>
             <div className="h-full w-[300px] bg-accent text-xl text-accent font-bold flex flex-col">
                 <Link className={getClass("products")} to="/adminPage/products">Products</Link>
                 <Link className={getClass("orders")} to="/adminPage/orders">Orders</Link>
@@ -27,13 +61,17 @@ export default function AdminPage(){
             </div>
             <div className="h-full w-[calc(100%-300px)] bg-secondary border-primary border-4 rounded-xl flex flex-col overflow-hidden">
                 <Routes path="/*">
-                    <Route path="/products" element={<AdminProductPage/>} />
-                    <Route path="/orders" element={<AdminOrdersPage/>} />
+                    <Route path="/products" element={<AdminProductPage />} />
+                    <Route path="/orders" element={<AdminOrdersPage />} />
                     <Route path="/users" element={<h1>Users</h1>} />
-                    <Route path="/add-product" element={<AddProductPage/>}/>
-                    <Route path="/edit-product" element={<EditProductPage/>}/>
+                    <Route path="/add-product" element={<AddProductPage />} />
+                    <Route path="/edit-product" element={<EditProductPage />} />
                 </Routes>
             </div>
+            </>
+        }
         </div>
+        
+        
     )
 }
